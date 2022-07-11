23878c55a9246803679f2c10b31c8e9fa1a9d27e
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2022-07-11 11:04:27 -0500

    Definiendo ubicación de despliegue de Eureka y Creando el servicio de Ventas.

---
+++ ms1_ventas/src/main/java/com/ventas/Ms1VentasApplication.java
@@ -0,0 +1,13 @@
+package com.ventas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ms1VentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ms1VentasApplication.class, args);
+	}
+
+}
