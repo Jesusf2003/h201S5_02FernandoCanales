218f140ccb08e19c3e0d269b244d5548f8c1c167
unknown <sebastian.samaniego@vallegrande.edu.pe>
2022-07-12 15:17:30 -0500

    Limpiando rama

--- ms3_stock/src/main/java/com/stock/domain/VentaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.stock.domain;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentaRepository extends MongoRepository<Venta, String>{
-}
