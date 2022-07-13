0a5393a0af5ba321c2c32b4166fff4ca291c033c
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2022-07-12 21:21:49 -0500

    Cambios en el README.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Azure ML - Frontend
+
+Para crear un proyecto de Angular desde una carpeta existente se ejecuta el siguiente comando:
+
+> ng new *nombre del proyecto* --directory ./
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
